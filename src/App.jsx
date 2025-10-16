99c249c050e3ed4cf0844322a4aa09f6a5dfd81a
bhairavaa <manikantaa.prakash@gmail.com>
2025-10-16 14:21:25 +0530

    commiting common files

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route}  from 'react-router-dom';
+import Login from './pages/login';
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
+
+
+      </Routes>
+    </Router>
       
     </>
   )
 }
 
 export default App
+
+
+
+ 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import {useNavigate} from 'react-router-dom';
+
+function Login(){
+    const navigate = useNavigate();
+    return(
+        <>
+            <div className="container-login">
+                <div className="login-div">
+                    <h1>Hi This is Login page</h1>
+                    <h2>dsfbjjhkj</h2> 
+                    <button onClick={() => {navigate('/home')}}>Click here to go to Home Page</button>                    
+                </div>
+
+            </div>
+            
+                   
+        </>    
+    );    
+}
+
+export default Login;
+
